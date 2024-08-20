2f98bd896af2bc950f4d206edad620809d7fa94e
FabioStorer <fabiostorer19@gmail.com>
2024-08-20 11:14:48 -0300

    feat: testado

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const dono_controller = require("./controllers/dono.js");
 const cliente_controller = require("./controllers/cliente.js");
 const loja_controller = require('./controllers/loja.js');
 const gerente_controller = require('./controllers/gerente.js');
-const endereco_loja_controller = require('./controllers/endereco_loja.js');
+const endereco_controller = require('./controllers/endereco.js');
 const financeiro_controller = require('./controllers/financeiro.js')
 const produto_controller = require('./controllers/produto.js')
 const estoque_controller = require('./controllers/estoque.js')
@@ -19,23 +19,23 @@ app.use(express.json());
 app.post("/dono", (req, res) => {
     const dono = req.body
     const code = dono_controller.store(dono)
-    res.status(code).json()
+    res.status(code).json();
 });
 
 app.get("/dono", (req, res) => {
-    const donos = dono_controller.index()
-    res.json(donos)
+    const dono = dono_controller.index()
+    res.json(dono)
 });
 
 app.get("/dono/:id", (req, res) => {
-    const dono = dono_controller.show(req.params.id)
+    const dono = dono_controller.show(req.params.id, req.body)
     res.json(dono)
 });
 
 app.put("/dono/:id", (req, res) => {
     const dono = req.body
     const code = dono_controller.update(req.params.id, dono)
-    res.status(code).json()
+    res.status(code).json();
 });
 
 app.delete("/dono/:id", (req, res) => {
@@ -48,7 +48,7 @@ app.delete("/dono/:id", (req, res) => {
 app.post("/fornecedor", (req, res) => {
     const fornecedor = req.body
     const code = fornecedor_controller.store(fornecedor)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/fornecedor", (req, res) => {
@@ -57,44 +57,46 @@ app.get("/fornecedor", (req, res) => {
 });
 
 app.get("/fornecedor/:id", (req, res) => {
-    const fornecedor = fornecedor_controller.update(req.params.id, fornecedor)
+    const fornecedor = fornecedor_controller.show(req.params.id, req.body)
     res.json(fornecedor)
 });
 
 app.put("/fornecedor/:id", (req, res) => {
     const fornecedor = req.body
     const code = fornecedor_controller.update(req.params.id, fornecedor)
+    res.status(code).json();
 });
 
-app.delete("fornecedor/:id", (req, res) => {
+app.delete("/fornecedor/:id", (req, res) => {
     fornecedor_controller.destroy(req.params.id)
     res.json()
 });
 
 // GERENCIAMENTO CLIENTE //
 
-app.post("/clientes", (req, res) => {
+app.post("/cliente", (req, res) => {
     const cliente = req.body
     const code = cliente_controller.store(cliente)
-    res.status(code)
+    res.status(code).json();
 });
 
-app.get("/clientes", (req, res) => {
+app.get("/cliente", (req, res) => {
     const cliente = cliente_controller.index()
     res.json(cliente)
 });
 
-app.get("/clientes/:id", (req, res) => {
-    const cliente = cliente_controller.update(req.params.id, cliente)
+app.get("/cliente/:id", (req, res) => {
+    const cliente = cliente_controller.show(req.params.id, req.body)
     res.json(cliente)
 });
 
-app.put("/clientes/:id", (req, res) => {
+app.put("/cliente/:id", (req, res) => {
     const cliente = req.body
     const code = cliente_controller.update(req.params.id, cliente)
+    res.status(code).json();
 });
 
-app.delete("cliente/:id", (req, res) => {
+app.delete("/cliente/:id", (req, res) => {
     cliente_controller.destroy(req.params.id)
     res.json()
 });
@@ -104,7 +106,7 @@ app.delete("cliente/:id", (req, res) => {
 app.post("/loja", (req, res) => {
     const loja = req.body
     const code = loja_controller.store(loja)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/loja", (req, res) => {
@@ -113,16 +115,17 @@ app.get("/loja", (req, res) => {
 });
 
 app.get("/loja/:id", (req, res) => {
-    const loja = loja_controller.update(req.params.id, loja)
+    const loja = loja_controller.show(req.params.id, req.body)
     res.json(loja)
 });
 
 app.put("/loja/:id", (req, res) => {
     const loja = req.body
     const code = loja_controller.update(req.params.id, loja)
+    res.status(code).json();
 });
 
-app.delete("loja/:id", (req, res) => {
+app.delete("/loja/:id", (req, res) => {
     loja_controller.destroy(req.params.id)
     res.json()
 });
@@ -132,7 +135,7 @@ app.delete("loja/:id", (req, res) => {
 app.post("/gerente", (req, res) => {
     const gerente = req.body
     const code = gerente_controller.store(gerente)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/gerente", (req, res) => {
@@ -141,16 +144,17 @@ app.get("/gerente", (req, res) => {
 });
 
 app.get("/gerente/:id", (req, res) => {
-    const gerente = gerente_controller.update(req.params.id, gerente)
+    const gerente = gerente_controller.show(req.params.id, req.body)
     res.json(gerente)
 });
 
 app.put("/gerente/:id", (req, res) => {
     const gerente = req.body
     const code = gerente_controller.update(req.params.id, gerente)
+    res.status(code).json();
 });
 
-app.delete("gerente/:id", (req, res) => {
+app.delete("/gerente/:id", (req, res) => {
     gerente_controller.destroy(req.params.id)
     res.json()
 });
@@ -159,32 +163,29 @@ app.delete("gerente/:id", (req, res) => {
 
 app.post("/endereco", (req, res) => {
     const endereco = req.body
-    const code = endereco_loja_controller.store(endereco)
-    res.status(code)
+    const code = endereco_controller.store(endereco)
+    res.status(code).json();
 });
 
 app.get("/endereco", (req, res) => {
-    const endereco = endereco_loja_controller.index()
+    const endereco = endereco_controller.index()
     res.json(endereco)
 });
 
 app.get("/endereco/:id", (req, res) => {
-    const endereco = endereco_loja_controller.update(req.params.id, endereco)
+    const endereco = endereco_controller.show(req.params.id, req.body)
     res.json(endereco)
 });
 
 app.put("/endereco/:id", (req, res) => {
     const endereco = req.body
-    const code = endereco_loja_controller.update(req.params.id, endereco)
+    const code = endereco_controller.update(req.params.id, endereco)
+    res.status(code).json();
 });
 
-app.delete("endereco/:id", (req, res) => {
-    endereco_loja_controller.destroy(req.params.id)
+app.delete("/endereco/:id", (req, res) => {
+    endereco_controller.destroy(req.params.id)
     res.json()
-});
-
-app.listen(port, () => {
-    console.log("servidor rodando")
 });
 
 // GERENCIAMENTO FINANCEIRO //
@@ -192,7 +193,7 @@ app.listen(port, () => {
 app.post("/financeiro", (req, res) => {
     const financeiro = req.body
     const code = financeiro_controller.store(financeiro)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/financeiro", (req, res) => {
@@ -201,16 +202,17 @@ app.get("/financeiro", (req, res) => {
 });
 
 app.get("/financeiro/:id", (req, res) => {
-    const financeiro = financeiro_controller.update(req.params.id, financeiro)
+    const financeiro = financeiro_controller.show(req.params.id, req.body)
     res.json(financeiro)
 });
 
 app.put("/financeiro/:id", (req, res) => {
     const financeiro = req.body
     const code = financeiro_controller.update(req.params.id, financeiro)
+    res.status(code).json();
 });
 
-app.delete("financeiro/:id", (req, res) => {
+app.delete("/financeiro/:id", (req, res) => {
     financeiro_controller.destroy(req.params.id)
     res.json()
 });
@@ -220,7 +222,7 @@ app.delete("financeiro/:id", (req, res) => {
 app.post("/produto", (req, res) => {
     const produto = req.body
     const code = produto_controller.store(produto)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/produto", (req, res) => {
@@ -229,16 +231,17 @@ app.get("/produto", (req, res) => {
 });
 
 app.get("/produto/:id", (req, res) => {
-    const produto = produto_controller.update(req.params.id, produto)
+    const produto = produto_controller.show(req.params.id, req.body)
     res.json(produto)
 });
 
 app.put("/produto/:id", (req, res) => {
     const produto = req.body
     const code = produto_controller.update(req.params.id, produto)
+    res.status(code).json();
 });
 
-app.delete("produto/:id", (req, res) => {
+app.delete("/produto/:id", (req, res) => {
     produto_controller.destroy(req.params.id)
     res.json()
 });
@@ -248,7 +251,7 @@ app.delete("produto/:id", (req, res) => {
 app.post("/estoque", (req, res) => {
     const estoque = req.body
     const code = estoque_controller.store(estoque)
-    res.status(code)
+    res.status(code).json();
 });
 
 app.get("/estoque", (req, res) => {
@@ -257,16 +260,21 @@ app.get("/estoque", (req, res) => {
 });
 
 app.get("/estoque/:id", (req, res) => {
-    const estoque = estoque_controller.update(req.params.id, estoque)
+    const estoque = estoque_controller.show(req.params.id, req.body)
     res.json(estoque)
 });
 
 app.put("/estoque/:id", (req, res) => {
     const estoque = req.body
     const code = estoque_controller.update(req.params.id, estoque)
+    res.status(code).json();
 });
 
 app.delete("/estoque/:id", (req, res) => {
     estoque_controller.destroy(req.params.id)
     res.json()
 });
+
+app.listen(port, () => {
+    console.log('Servidor iniciado.');
+})
